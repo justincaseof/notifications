5b635a3385402faad22ea9c2f3661b4419ea4146
justincaseof <justin.caseof@gmail.com>
2016-12-11 13:55:38 +0100

    * test stuff

--- Application/src/main/java/com/example/android/customnotifications/Main2Activity.java
+++ Application/src/main/java/com/example/android/customnotifications/Main2Activity.java
@@ -1,16 +1,99 @@
 package com.example.android.customnotifications;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.URLUtil;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main2Activity extends Activity {
+
+    private static final String LOGTAG = "TOBI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("LOGTAG", "########################");
+        Log.d(LOGTAG, "########################");
+        try {
+            getStatus();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main2);
     }
+
+    private void getStatus() throws Exception {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Log.d(LOGTAG, "online!");
+            String ip = "192.168.178.48";
+            String target = "http://" + ip + "/status";
+            handleResponse(downloadUrl(target));
+        } else {
+            Log.d(LOGTAG, "offline!");
+        }
+
+
+    }
+
+    private void handleResponse(String s) {
+        Log.d(LOGTAG, "------------------------------------------------");
+        Log.d(LOGTAG, s);
+    }
+
+    private String downloadUrl(String myurl) throws Exception {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(5000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(LOGTAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws Exception {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
 }
