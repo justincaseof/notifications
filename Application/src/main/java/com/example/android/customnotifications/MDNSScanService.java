aeffe562c7577a595a9158d1fab9c39fe2be45d2
justincaseof <justin.caseof@gmail.com>
2016-12-28 15:35:22 +0100

    mDNS (working) scan example

--- Application/src/main/java/com/example/android/customnotifications/MDNSScanService.java
+++ Application/src/main/java/com/example/android/customnotifications/MDNSScanService.java
@@ -1,6 +1,7 @@
 package com.example.android.customnotifications;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
@@ -15,10 +16,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static android.content.ContentValues.TAG;
 
 public class MDNSScanService extends IntentService {
 
@@ -26,6 +26,8 @@ public class MDNSScanService extends IntentService {
     public static final String LOGTAG = "MDNSScanService";
 
     private NsdManager.DiscoveryListener mDiscoveryListener;
+    private NsdManager.ResolveListener mResolveListener;
+    private NsdManager mNsdManager;
 
     public MDNSScanService() {
         super("MDNSScanService");
@@ -34,8 +36,46 @@ public class MDNSScanService extends IntentService {
     // will be called asynchronously by Android
     @Override
     protected void onHandleIntent(Intent intent) {
-        boolean updateOnly = intent.getBooleanExtra(ARGUMENT_BOOL_1, false);
+        boolean arg1 = intent.getBooleanExtra(ARGUMENT_BOOL_1, false);
+        initializeDiscoveryListener();
+        initializeResolveListener();
 
+        mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
+
+        /* Working:
+         *   --> _http._tcp
+         *   --> _http._tcp.
+         * NOT working:
+         *   --> _http._tcp.local
+         */
+        String service = "_http._tcp";
+        mNsdManager.discoverServices(service, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+    }
+
+    private void initializeResolveListener() {
+        mResolveListener = new NsdManager.ResolveListener() {
+            @Override
+            public void onResolveFailed(NsdServiceInfo nsdServiceInfo, int i) {
+                Log.d(LOGTAG, "onResolveFailed");
+            }
+
+            @Override
+            public void onServiceResolved(NsdServiceInfo nsdServiceInfo) {
+                Log.d(LOGTAG, "onServiceResolved");
+                Log.d(LOGTAG, "  .getServiceName() : " + nsdServiceInfo.getServiceName());
+                Log.d(LOGTAG, "  .getServiceType() : " + nsdServiceInfo.getServiceType());
+                Log.d(LOGTAG, "  .getHost() : " + nsdServiceInfo.getHost());
+                Log.d(LOGTAG, "  .getPort() : " + nsdServiceInfo.getPort());
+                Log.d(LOGTAG, "  .getAttributes() : ");
+                if(nsdServiceInfo.getAttributes()==null || nsdServiceInfo.getAttributes().size()<1 ) {
+                    Log.d(LOGTAG, "  --> NONE!");
+                } else {
+                    for (Map.Entry<String, byte[]> pair : nsdServiceInfo.getAttributes().entrySet()) {
+                        Log.d(LOGTAG, "  --> " + pair.getKey() + ":" + new String(pair.getValue()));
+                    }
+                }
+            }
+        };
     }
 
     public void initializeDiscoveryListener() {
@@ -44,38 +84,39 @@ public class MDNSScanService extends IntentService {
             //  Called as soon as service discovery begins.
             @Override
             public void onDiscoveryStarted(String regType) {
-                Log.d(TAG, "Service discovery started");
+                Log.d(LOGTAG, "Service discovery started");
             }
 
             @Override
             public void onServiceFound(NsdServiceInfo service) {
                 // A service was found!  Do something with it.
-                Log.d(TAG, "Service discovery success" + service);
+                Log.d(LOGTAG, "Service discovery success" + service);
 
+                mNsdManager.resolveService(service, mResolveListener);
             }
 
             @Override
             public void onServiceLost(NsdServiceInfo service) {
                 // When the network service is no longer available.
                 // Internal bookkeeping code goes here.
-                Log.e(TAG, "service lost" + service);
+                Log.e(LOGTAG, "service lost" + service);
             }
 
             @Override
             public void onDiscoveryStopped(String serviceType) {
-                Log.i(TAG, "Discovery stopped: " + serviceType);
+                Log.i(LOGTAG, "Discovery stopped: " + serviceType);
             }
 
             @Override
             public void onStartDiscoveryFailed(String serviceType, int errorCode) {
-                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
-                //mNsdManager.stopServiceDiscovery(this);
+                Log.e(LOGTAG, "Discovery failed: Error code:" + errorCode);
+                mNsdManager.stopServiceDiscovery(this);
             }
 
             @Override
             public void onStopDiscoveryFailed(String serviceType, int errorCode) {
-                Log.e(TAG, "Discovery failed: Error code:" + errorCode);
-                //mNsdManager.stopServiceDiscovery(this);
+                Log.e(LOGTAG, "Discovery failed: Error code:" + errorCode);
+                mNsdManager.stopServiceDiscovery(this);
             }
         };
     }
